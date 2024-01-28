b5327fba24c611cb52ba945cc5031e07142622cf
jacksonzha0 <zjk13917959549>
2024-01-29 00:10:56 +0800

    chatbot-init

---
+++ chatbot-interface/src/main/java/cn/zjk/chatbot/api/Application.java
@@ -0,0 +1,15 @@
+package cn.zjk.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zjk
+ * @CreateTime 2024/1/28 23:48
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
